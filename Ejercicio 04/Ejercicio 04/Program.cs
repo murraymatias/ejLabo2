e473852035211c79f6bc8c8b1405a18f2bb7ecbd
murraymatias <murray.matias@gmail.com>
2019-08-22 19:59:55 -0300

    Ejercicio 06 finalizado y comentado

--- Ejercicio 04/Ejercicio 04/Program.cs
+++ Ejercicio 04/Ejercicio 04/Program.cs
@@ -7,7 +7,8 @@ using System.Threading.Tasks;
 (excluido el mismo) que son divisores del número.
 El primer número perfecto es 6, ya que los divisores de 6 son 1, 2 y 3; y 1 + 2 + 3 = 6.
 Escribir una aplicación que encuentre los 4 primeros números perfectos.
-Nota: Utilizar estructuras repetitivas y selectivas.*/
+Nota: Utilizar estructuras repetitivas y selectivas.
+*/
 namespace Ejercicio_04
 {
     class Program
---
+++ Ejercicio 06/Ejercicio 06/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*Escribir un programa que determine si un año es bisiesto.
+Un año es bisiesto si es múltiplo de 4. Los años múltiplos de 100 no son bisiestos, salvo si ellos
+también son múltiplos de 400. Por ejemplo: el año 2000 es bisiesto pero 1900 no.
+Pedirle al usuario un año de inicio y otro de fin y mostrar todos los años bisiestos en ese rango.
+Nota: Utilizar estructuras repetitivas, selectivas y la función módulo (%).*/
+namespace Ejercicio_06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int initYear, endYear;
+
+            Console.WriteLine("Ingrese año de inicio: ");
+            initYear = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ingrese año de finalizacion: ");
+            endYear = Convert.ToInt32(Console.ReadLine());
+
+            //Itera por todos los numeros enteros entre los valores ingresados.
+            for (int i = initYear; i<=endYear; i++)
+            {
+                //Prueba si es multiplo de 4.
+                if(i % 4 == 0)
+                {
+                    //Comprueba que no sea multiplo de 100.
+                    if(i % 100 != 0)
+                    {
+                        Console.WriteLine("El año {0} es bisiesto.", i);
+                    }
+                    else
+                    {
+                        //Si es multiplo de 100, prueba que tambien lo sea de 400.
+                        if(i % 400 == 0)
+                        {
+                            Console.WriteLine("El año {0} es bisiesto.", i);
+                        }
+                    }
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
