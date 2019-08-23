c451ec5f8040620daed32a2e108cb11825c59a6a
murraymatias <murray.matias@gmail.com>
2019-08-22 21:51:38 -0300

    Ejercicio 07 a medio hacer sin datetime, ejercicio 08 completo

--- Ejercicio 07/Ejercicio 07/Program.cs
+++ Ejercicio 07/Ejercicio 07/Program.cs
@@ -7,7 +7,8 @@ using System.Threading.Tasks;
 /*Hacer un programa que pida por pantalla la fecha de nacimiento de una persona (día, mes y año) y
 calcule el número de días vividos por esa persona hasta la fecha actual (tomar la fecha del sistema
 con DateTime.Now).
-Nota: Utilizar estructuras selectivas. Tener en cuenta los años bisiestos.*/
+Nota: Utilizar estructuras selectivas. Tener en cuenta los años bisiestos.
+*/
 namespace Ejercicio_07
 {
     class Program
@@ -15,25 +16,56 @@ namespace Ejercicio_07
         static void Main(string[] args)
         {
             int year, month, day;
+            int thisYear, thisMonth, thisDay;
+            int cuentaDias=0;
 
             Console.WriteLine("Ingrese su año de nacimiento: ");
             year = Convert.ToInt32(Console.ReadLine());
-
             Console.WriteLine("Ingrese su mes de nacimiento: ");
             month = Convert.ToInt32(Console.ReadLine());
-
             Console.WriteLine("Ingrese su dia de nacimiento: ");
             day = Convert.ToInt32(Console.ReadLine());
 
-            DateTime fechaNacimiento = new DateTime(year, month, day);
+            thisYear = (int)DateTime.Now.Year;
+            thisMonth = (int)DateTime.Now.Month;
+            thisDay = (int)DateTime.Now.Day;
 
-            DateTime fechaHoy = DateTime.Now;
+            for(int i = year; i<thisYear; i++)
+            {
+                if(isBisiesto(year))
+                {
+                    cuentaDias += 366;
+                }
+                else
+                {
+                    cuentaDias += 365;
+                }
+            }
 
-            int resultado = (int)fechaHoy.Subtract(fechaNacimiento).TotalDays;
-
-            Console.WriteLine("Usted tiene {0} dias de edad", resultado);
 
             Console.ReadKey();
+        }
+
+        private static bool isBisiesto(int year)
+        {
+            bool isBisiesto = false;
+            if (year % 4 == 0)
+            {
+                //Comprueba que no sea multiplo de 100.
+                if (year % 100 != 0)
+                {
+                    isBisiesto = true;
+                }
+                else
+                {
+                    //Si es multiplo de 100, prueba que tambien lo sea de 400.
+                    if (year % 400 == 0)
+                    {
+                        isBisiesto = true;
+                    }
+                }
+            }
+            return isBisiesto;
         }
     }
 }
---
+++ Ejercicio 08/Ejercicio 08/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*. Por teclado se ingresa el valor hora, el nombre, la antigüedad (en años) y la cantidad de horas
+trabajadas en el mes de N empleados de una fábrica.
+Se pide calcular el importe a cobrar teniendo en cuenta que el total (que resulta de multiplicar el
+valor hora por la cantidad de horas trabajadas), hay que sumarle la cantidad de años trabajados
+multiplicados por $ 150, y al total de todas esas operaciones restarle el 13% en concepto de
+descuentos.
+Mostrar el recibo correspondiente con el nombre, la antigüedad, el valor hora, el total a cobrar en
+bruto, el total de descuentos y el valor neto a cobrar de todos los empleados ingresados.
+Nota: Utilizar estructuras repetitivas y selectivas
+*/
+namespace Ejercicio_08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            float valorHora;
+            int antiguedad;
+            int horasTrabajadas;
+            float sueldoBruto;
+            float sueldoNeto;
+            float descuentos;
+            char respuesta;
+
+            do { 
+                Console.WriteLine("Ingrese nombre: ");
+                nombre = Console.ReadLine();
+                Console.WriteLine("Ingrese valor hora: ");
+                valorHora = Convert.ToSingle(Console.ReadLine());
+                Console.WriteLine("Ingrese antiguedad: ");
+                antiguedad = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Ingrese horas trabajadas: ");
+                horasTrabajadas = Convert.ToInt32(Console.ReadLine());
+
+                sueldoBruto = horasTrabajadas * valorHora + antiguedad * 150;
+                descuentos = sueldoBruto * (float)0.13;
+                sueldoNeto = sueldoBruto * (float)0.87;
+
+                Console.WriteLine("Nombre: {0}", nombre);
+                Console.WriteLine("Antiguedad: {0}", antiguedad);
+                Console.WriteLine("Valor hora: {0}", valorHora);
+                Console.WriteLine("Sueldo bruto: {0}", sueldoBruto);
+                Console.WriteLine("Descuentos: {0}", descuentos);
+                Console.WriteLine("Sueldo neto: {0}", sueldoNeto);
+                Console.WriteLine();
+                Console.WriteLine("Desea volver a calcular [S/N]:");
+                respuesta = Convert.ToChar(Console.ReadLine());
+
+            }while(respuesta == 'S');
+
+
+        }
+    }
+}
