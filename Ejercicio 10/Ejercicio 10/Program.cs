c08a547edcabe59189d6cf738a2a0360d81803e4
murraymatias <murray.matias@gmail.com>
2019-08-24 21:35:13 -0300

    Ejericio 10 finalizado

---
+++ Ejercicio 10/Ejercicio 10/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int levels;
+            string step = "*";
+
+            Console.WriteLine("Ingrese el numero de niveles:");
+            levels = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = levels; i>0; i--)
+            {
+                Console.WriteLine(Indent(i) + step);
+                step = step + "**";
+            }
+
+            Console.ReadKey();
+        }
+
+        public static string Indent(int count)
+        {
+            return "".PadLeft(count);
+        }
+    }
+}
