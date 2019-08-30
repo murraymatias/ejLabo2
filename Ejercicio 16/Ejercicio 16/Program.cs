9f5ec100796ea4248f415b6229adc275d5732071
murraymatias <murray.matias@gmail.com>
2019-08-29 21:30:13 -0300

    Ejercicio 16 finalizado

---
+++ Ejercicio 16/Ejercicio 16/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+
+namespace Ejercicio_16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Alumno alumno1 = new Alumno("Lorem", "Sit", 1);
+            alumno1.Estudiar(6, 3);
+            alumno1.CalcularFinal();
+            Console.WriteLine(alumno1.Mostrar());
+
+            Alumno alumno2 = new Alumno("Ipsum", "Amet", 2);
+            alumno2.Estudiar(4, 4);
+            alumno2.CalcularFinal();
+            Console.WriteLine(alumno2.Mostrar());
+
+            Alumno alumno3 = new Alumno("Dolor", "Consectetur", 3);
+            alumno3.Estudiar(6, 6);
+            alumno3.CalcularFinal();
+            Console.WriteLine(alumno3.Mostrar());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Ejercicio 16/Entidades/Alumno.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Alumno
+    {
+        private byte nota1;
+        private byte nota2;
+        private float notaFinal;
+        public string apellido;
+        public int legajo;
+        public string nombre;
+
+        public Alumno(string nombre, string apellido, int legajo)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.legajo = legajo;
+        }   
+
+        public void CalcularFinal()
+        {
+            if(this.nota1>=4 && this.nota2>=4)
+            {
+                Random rnd = new Random();
+                this.notaFinal = rnd.Next(1, 11);
+            }
+            else
+            {
+                this.notaFinal = -1;
+            }
+        }
+        
+        public void Estudiar(byte notaUno, byte notaDos)
+        {
+            this.nota1 = notaUno;
+            this.nota2 = notaDos;
+        }
+
+        public string Mostrar()
+        {
+            string ret = String.Format(
+                    "Legajo: {0}\n" +
+                    "Nombre: {1}\n" +
+                    "Apellido: {2}\n" +
+                    "Nota1: {3}\n" +
+                    "Nota2: {4}\n" +
+                    "Nota Final: ", this.legajo, this.nombre, this.apellido, this.nota1, this.nota2);
+
+            if (this.notaFinal != -1)
+            {
+                ret += Convert.ToString(this.notaFinal)+"\n";
+            }
+            else
+            {
+                ret += "Alumno desaprobado\n";
+            }
+                return ret;
+        }
+    }
+}
