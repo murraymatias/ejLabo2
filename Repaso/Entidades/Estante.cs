ce857a6651a38668f34a5d6ae8ed63228cc2da21
murraymatias <murray.matias@gmail.com>
2019-09-04 22:07:37 -0300

    ejercicio clase 5 para finalizar

---
+++ Repaso/Entidades/Estante.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Estante
+    {
+        private Producto[] productos;
+        private int ubicacionEstante;
+
+        private Estante(int capacidad)
+        {
+            this.productos = new Producto[capacidad];
+        }
+
+        public Estante(int capacidad,int ubicacionEstante):this(capacidad)
+        {
+            this.ubicacionEstante = ubicacionEstante;
+        }
+
+        public Producto[] GetProductos()
+        {
+            return this.productos;
+        }
+
+        public static string MostrarEstante(Estante e)
+        {
+            string ret = string.Format("Ubicacion del estante: {0}\nProductos:\n",e.ubicacionEstante);
+
+            foreach (Producto p in e.GetProductos())
+            {
+                ret += Producto.MostrarProducto(p);
+            }
+            return ret;
+        }
+
+        public static bool operator ==(Estante e,Producto p)
+        {
+            foreach(Producto p1 in e.GetProductos())
+            {
+                if(p1 == p)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static bool operator !=(Estante e, Producto p)
+        {
+            foreach (Producto p1 in e.GetProductos())
+            {
+                if (p1 == p)
+                {
+                    
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public static bool operator +(Estante e, Producto p)
+        {
+            Producto[] aux = e.GetProductos();
+            if(e!=p)
+            {
+                for(int i=0;i>aux.Length;i++)
+                {
+                    if(aux[i] is null)
+                    {
+                        aux[i] = p;
+                    }
+                }
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
---
+++ Repaso/Entidades/Producto.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Producto
+    {
+        private string marca;
+        private string codigoDeBarras;
+        private float precio;
+
+        public Producto(string marca, string codigo, float precio)
+        {
+            this.marca = marca;
+            this.codigoDeBarras = codigo;
+            this.precio = precio;
+        }
+
+        public string GetMarca()
+        {
+            return this.marca;
+        }
+
+        public float GetPrecio()
+        {
+            return this.precio;
+        }
+
+        public static string MostrarProducto(Producto p)
+        {
+             return string.Format(
+                "Marca: {0}\n" +
+                "Precio: {1}\n" +
+                "Codigo de barras: {2}", p.marca, p.precio, p.codigoDeBarras);
+
+        }
+
+        public static explicit operator string(Producto p)
+        {
+            return p.codigoDeBarras;
+        }
+
+        public static bool operator ==(Producto p1, Producto p2)
+        {
+            if (p1.codigoDeBarras == p2.codigoDeBarras && p1.marca == p2.marca)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool operator !=(Producto p1, Producto p2)
+        {
+            if (p1.codigoDeBarras != p2.codigoDeBarras || p1.marca != p2.marca)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool operator ==(Producto p, string marca)
+        {
+            if(p.marca == marca)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool operator !=(Producto p, string marca)
+        {
+            if(p.marca != marca)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
