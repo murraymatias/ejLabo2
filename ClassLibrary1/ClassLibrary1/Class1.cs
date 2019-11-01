54d562d0886aa881b2901ba2791382b5c2bc0536
alumno <alumno@utnfra.com.ar>
2019-10-31 22:20:19 -0300

    coneccion bases de datos

---
+++ ClassLibrary1/ClassLibrary1/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace ClassLibrary1
+{
+    public class DAO
+    {
+        private static SqlConnection connection;
+        private static SqlCommand command;
+
+        static DAO()
+        {
+            string connectionString = @"Data source = .\ ; Initial Catalog = BDVet; Integrated Security=true;";
+            connection = new SqlConnection(connectionString);
+            command = new SqlCommand();
+            command.Connection = connection;
+            command.CommandType = System.Data.CommandType.Text;
+        }
+
+        public static void InsertarCliente(string nombre,string apellido, string dni)
+        {
+            try
+            {
+                connection.Open();
+                string comando = string.Format("INSERT INTO CLIENTES (nombre,apellido,dni) VALUES ('{0}','{1}','{2}')",nombre,apellido,dni);
+
+                command.CommandText = comando;
+                command.ExecuteNonQuery();
+            }
+            finally
+            {
+                if(connection.State == System.Data.ConnectionState.Open && connection != null)
+                {
+                    connection.Close();
+                }
+            }
+        }
+    }
+}
---
+++ ClassLibrary1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                DAO.InsertarCliente("Lorem", "Ipsum", "123456");
+            }
+            finally
+            {
+
+            }
+        }
+    }
+}
