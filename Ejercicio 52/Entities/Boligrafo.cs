4f8b9a3e749c8cef9b68c2ee120c79c4c48cb97f
murraymatias <murray.matias@gmail.com>
2019-11-03 14:47:13 -0300

    Actualizacion repositorio

---
+++ Ejercicio 52/Entities/Boligrafo.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Boligrafo : IAcciones
+    {
+        private ConsoleColor _colorTinta;
+        private float _tinta;
+
+        public ConsoleColor Color
+        {
+            get => this._colorTinta;
+            set => this._colorTinta = value;
+        }
+
+        public float UnidadesDeEscritura
+        {
+            get => this._tinta;
+            set => this._tinta = value;
+        }
+
+        public Boligrafo(int unidades, ConsoleColor color)
+        {
+            this._colorTinta = color;
+            this._tinta = unidades;
+        }
+
+        public EscrituraWrapper Escribir(string texto)
+        {
+            string writhedText = null;
+
+            foreach (char c in texto)
+            {
+                if (_tinta <= 0) break;
+
+                writhedText += c;
+
+                if(c == ' ') continue;
+
+                this._tinta -= (float)0.3;
+            }
+
+            return  new EscrituraWrapper(writhedText,this.Color);
+        }
+
+        public bool Recargar(int unidades)
+        {
+            this._tinta += unidades;
+
+            return true;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("Boligrafo");
+            sb.AppendFormat("Color: {0}", this.Color.ToString());
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ Ejercicio 52/Entities/Lapiz.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Lapiz : IAcciones
+    {
+        private float tamanioMina;
+
+        public Lapiz(int unidades)
+        {
+            this.tamanioMina = unidades;
+        }
+
+        ConsoleColor IAcciones.Color
+        {
+            get => ConsoleColor.Gray;
+            set => throw  new NotImplementedException();
+        }
+
+        float IAcciones.UnidadesDeEscritura
+        {
+            get => this.tamanioMina;
+            set => this.tamanioMina = value;
+        }
+
+        EscrituraWrapper IAcciones.Escribir(string texto)
+        {
+            string writhedText = null;
+
+            foreach (char c in texto)
+            {
+                if (this.tamanioMina <= 0) break;
+
+                writhedText += c;
+
+                if (c == ' ') continue;
+
+                this.tamanioMina -= (float)0.1;
+            }
+
+            return new EscrituraWrapper(writhedText, ConsoleColor.Gray);
+        }
+
+        bool IAcciones.Recargar(int unidades) { throw  new NotImplementedException();}
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("Lapiz");
+            sb.AppendFormat("Color: {0}", ((IAcciones)this).Color.ToString());
+
+            return sb.ToString();
+        }
+    }
+}
